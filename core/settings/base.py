cdd7b3d917103b1cd23242c0a1158214e10b391c
khiem <khiem3t@gmail.com>
2019-06-12 00:36:40 +0700

    add password db

--- core/settings/base.py
+++ core/settings/base.py
@@ -86,6 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': '5432',
     }
